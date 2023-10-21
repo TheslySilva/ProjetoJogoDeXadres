f93dcfbb70a9f404263f7fa20d51fcf841a42037
Thesly Silva <thesley456silva@gmail.com>
2023-10-21 08:25:50 -0300

    Test

--- src/program/Main.java
+++ src/program/Main.java
@@ -8,5 +8,7 @@ public class Main {
 	public static void main(String[] args) {
 		PartidaDeXadrez partidadexadrez = new PartidaDeXadrez();
 		UI.imprimirTabuleiro(partidadexadrez.getPecas());
+		
+		System.out.println("Fazendo um teste ");
 	}
 }
